628b395df17def0b2b7c8433c0a85a74d04a666a
clew <623056206@qq.com>
2020-02-09 19:52:13 +0800

    注释整理

--- src/bee-send.js
+++ src/bee-send.js
@@ -1,4 +1,7 @@
 /* eslint-disable no-param-reassign */
+/**
+ * bee 网络相关功能
+ */
 import config from './config';
 import {
   _,
@@ -10,7 +13,14 @@ const {
 } = window.navigator;
 const protocol = window.document.location.protocol === 'https:' ? 'https://' : 'http://';
 
+/**
+ * 这里计划在浏览器支持的情况下全部改为 navigator。sendBeacon 上报，
+ * 可能会通过函数柯理化实现
+ */
 export default function initSend(Bee) {
+  /**
+   * 普通上报事件
+   */
   Bee.prototype.send = function (d, async = true) {
     let xhr = null;
     if (typeof XMLHttpRequest !== 'undefined') {
@@ -19,16 +29,16 @@ export default function initSend(Bee) {
       // eslint-disable-next-line no-undef
       xhr = new ActiveXObject('Microsoft.XMLHTTP');
     }
-    // 跨域允许发送 cookie
-    // xhr.withCredentials = true;
     xhr.open('POST', protocol + config.req, async);
-    // xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded;charset=utf-8');
     xhr.setRequestHeader('Content-Type', 'text/plain');
     const data = _.checkRCFL(d);
     console.log('埋点上报数据：', '类型', data.t, '数据', data);
     xhr.send(JSON.stringify(data));
     this.beeData.updateCookieDate();
   };
+  /**
+   * sendBeacon 上报
+   */
   Bee.prototype.sendBean = function (d) {
     if (!_.isFunction(sendBeacon)) {
       return this.send(d, false);
